7ee6906b1e78f0bdcc8148ca283693ec462b3eb6
pyan83 <puyanwei@gmail.com>
2017-10-29 20:39:51 +0000

    function expressions

---
+++ app.js
@@ -0,0 +1,21 @@
+// normal function statement
+//
+// function sayHi() {
+//   console.log("hi");
+// }
+//
+// sayHi();
+//
+// // function expression
+//
+var sayBye = function() {
+  console.log("bye");
+};
+
+// sayBye();
+
+function callFunction(fun) {
+  fun();
+}
+
+callFunction(sayBye);
