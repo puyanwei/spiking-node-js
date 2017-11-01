14c7791828984219983a5c99f89c28c2f0f438ff
pyan83 <puyanwei@gmail.com>
2017-11-01 17:34:32 +0000

    routing with node

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-var http = require('http')
-var fs = require('fs')
-
-
+var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer((req, res) => {
   console.log('request was made: ' + req.url);
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-
-  var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8')
-  myReadStream.pipe(res)
+  if (req.url === '/home' || req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
+  } else if (req.url === '/contact') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.createReadStream(__dirname + '/contact.html').pipe(res);
+  } else if (req.url === '/api') {
+    var api = [{ name: 'bob', age: 33, sex: 'yes please' }];
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(api));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    fs.createReadStream(__dirname + '/404.html').pipe(res);
+  }
 });
 
 server.listen(3000, '127.0.0.1');
