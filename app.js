e366418cd5c881ced3af6aa77f6386144cb223d8
pyan83 <puyanwei@gmail.com>
2017-11-01 15:26:18 +0000

    pipes

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var http = require('http')
 var fs = require('fs')
 
-var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8')
-var myWriteStream = fs.createWriteStream(__dirname + '/writeme.txt')
 
-myReadStream.on('data', (chunk) => {
-  console.log('new chunk recieved:');
-  myWriteStream.write(chunk)
+
+var server = http.createServer((req, res) => {
+  console.log('request was made: ' + req.url);
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8')
+  myReadStream.pipe(res)
 });
 
-// allows data transfers in smaller bits
+server.listen(3000, '127.0.0.1');
+console.log('the server is listening to port 3000');
