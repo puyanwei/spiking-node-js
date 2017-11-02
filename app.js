a3ac2c2983eb7b2a896507a68ec5405c25c0588e
pyan83 <puyanwei@gmail.com>
2017-11-02 15:48:02 +0000

    routing parameters

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
 
-var server = http.createServer((req, res) => {
-  console.log('request was made: ' + req.url);
-  if (req.url === '/home' || req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.createReadStream(__dirname + '/index.html').pipe(res);
-  } else if (req.url === '/contact') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.createReadStream(__dirname + '/contact.html').pipe(res);
-  } else if (req.url === '/api') {
-    var api = [{ name: 'bob', age: 33, sex: 'yes please' }];
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(api));
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/html' });
-    fs.createReadStream(__dirname + '/404.html').pipe(res);
-  }
+var app = express();
+
+app.get('/', (req, res) => {
+  res.send('welcome to the my awesome webpage');
 });
 
-server.listen(3000, '127.0.0.1');
-console.log('the server is listening to port 3000');
+app.get('/contact', (req, res) => {
+  res.send('contact me for some swaaaag');
+});
+
+app.get('/profile/:id', (req, res) => {
+  res.send('You requested to see id profile ' + req.params.id);
+});
+
+app.listen(3000);
